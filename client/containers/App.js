345afdde637d236b8202d06e8c12158436e849cd
Zack Bostian <bostianz@gmail.com>
2016-02-09 09:52:36 -0500

    added my changes

--- client/containers/App.js
+++ client/containers/App.js
@@ -52,7 +52,6 @@ module.exports = React.createClass({
     formatInquiry: function(input) {
         return input.split(' ').join('+');
     },
-
     render: function() {
         return (
           <div>
---
+++ client/containers/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+var HomePage = React.createClass({
+  render: function() {
+    return (
+      
+    );
+  }
+});
+
+module.exports = HomePage;
